b6a74afdd6492db695103790a7cd8b3fe71f59a9
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-02-05 22:02:49 +0700

    Add Straight slot class

---
+++ lab4/task1/StraightSlot.java
@@ -0,0 +1,14 @@
+public class StraightSlot extends Slot {
+    SlotComponentFactory componentFactory;
+
+    public StraightSlot(SlotComponentFactory componentFactory) {
+        this.componentFactory= componentFactory;
+    }
+
+    void build() {
+        cabinet = componentFactory.createCabinet();
+        display = componentFactory.createDisplay();
+        gpu = componentFactory.createGPU();
+        payment = componentFactory.createPayment();
+    }
+}
