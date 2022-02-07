2bd863ff3e235744193453baafd3bf96aa7f635d
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-02-07 11:11:50 +0700

    Update WSSlotFactory.java

--- lab4/task1/WSSlotFactory.java
+++ lab4/task1/WSSlotFactory.java
@@ -1,18 +1,15 @@
 public class WSSlotFactory extends SlotFactory {
     protected Slot makeSlot(String item) {
         Slot slot = null;
-        SlotComponentFactory componentFactory = null;
+        SlotComponentFactory componentFactory = new WSSlotComponentFactory();
         if (item.equals("bonus")) {
-            componentFactory = new WSBonusComponentFactory();
             slot = new BonusSlot(componentFactory);
             slot.setName("Washington Style Bonus Slot");
         } else if (item.equals("progressive")) {
             slot = new ProgressiveSlot(componentFactory);
-            componentFactory = new WSProgressiveComponentFactory();
             slot.setName("Washington Style Progressive Slot");
         } else if (item.equals("straight")) {
             slot = new StraightSlot(componentFactory);
-            componentFactory = new WSStraightComponentFactory();
             slot.setName("Washington Style Straight Slot");
         }
         return slot;
