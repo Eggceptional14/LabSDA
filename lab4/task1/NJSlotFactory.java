d961d5d6190c460617baea773d2cb9595297aca6
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-02-07 11:17:02 +0700

    Update slot factory

--- lab4/task1/NJSlotFactory.java
+++ lab4/task1/NJSlotFactory.java
@@ -3,17 +3,14 @@ public class NJSlotFactory extends SlotFactory {
 
 	protected Slot makeSlot(String item) {
 		Slot slot = null;
-		SlotComponentFactory componentFactory = null;
+		SlotComponentFactory componentFactory = new NJComponentFactory();
 		if (item.equals("bonus")) {
-			componentFactory = new NJComponentFactory();
 			slot = new BonusSlot(componentFactory);
 			slot.setName("New Jersey Style Bonus Slot");
 		} else if (item.equals("progressive")) {
-			componentFactory = new NJComponentFactory();
 			slot = new ProgressiveSlot(componentFactory);
 			slot.setName("New Jersey Style Progressive Slot");
 		} else if (item.equals("straight")) {
-			componentFactory = new NJComponentFactory();
 			slot = new StraightSlot(componentFactory);
 			slot.setName("New Jersey Style Straight Slot");
 		}
