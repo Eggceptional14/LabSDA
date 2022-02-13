deea5fd2a1440609d4f8c51d516f8a3256921150
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-02-13 14:59:32 +0700

    Update calling method

--- lab5/task2/EatBreakfast.java
+++ lab5/task2/EatBreakfast.java
@@ -3,36 +3,15 @@ import java.time.Year;
 public class EatBreakfast {
 	public static void main(String[] args) {
 		ScrambledEggs scrambled = new ScrambledEggs();
-		scrambled.crackEggs(2);
-		scrambled.stirEggs();
-		scrambled.cook();
-		scrambled.serve();
-		if (scrambled.askCustomer()) {
-            scrambled.addSpread(); 
-        }
+		scrambled.TemplateMethod(2);
 
 
 		Omelette omelette = new Omelette();
-		omelette.crackEggs(3);
-		omelette.prepare();
-		omelette.cook();
-		omelette.serve();
-		if (omelette.askCustomer()) {
-            omelette.addSpread(); 
-        }
-
+		omelette.TemplateMethod(3);
 
 
 		SunnySide sunny = new SunnySide();
-		sunny.crackEggs(1);
-		sunny.prepare();
-		sunny.cook();
-		sunny.serve();
-		if (sunny.askCustomer()) {
-            sunny.addSpread(); 
-        }
-
-
+		sunny.TemplateMethod(1);
 	}
 
 }
