189705701709a022ad427fb0b0c4bb28ab16225b
Phurit Warapat <phuritwarapat@gmail.com>
2022-02-20 17:29:31 +0700

    update test db

--- lab6/task1/TestDatabase.java
+++ lab6/task1/TestDatabase.java
@@ -7,21 +7,21 @@ public class TestDatabase {
         Records rec = new Records();
         Database db2 = new Database();
 
-        List<Target> allContainer = new LinkedList<Target>();
+        List<DatabaseTarget> allContainer = new LinkedList<DatabaseTarget>();
         allContainer.add(new RecordsAdapter(rec));
         allContainer.add(new DatabaseAdapter(db2));
 
         Employee e1 = new Employee("AName", "ASurname", 1111, 120000);
         Employee e2 = new Employee("BName", "BSurname", 2222, 240000);
 
-        for (Target d : allContainer) {
+        for (DatabaseTarget d : allContainer) {
             d.addEmployee(e1);
             d.addEmployee(e2);
             d.printEmployees();
             System.out.println(d.isEmployee(1111));
         }
         System.out.println();
-        for (Target d : allContainer) {
+        for (DatabaseTarget d : allContainer) {
             d.removeEmployee(1111);
             d.printEmployees();
             System.out.println(d.isEmployee(1111));
