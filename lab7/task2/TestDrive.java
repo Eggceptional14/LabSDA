b705430d3d54942162339d56f4615e8b21d6bc3f
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-02-25 22:51:38 +0700

    Update TestDrive.java

--- lab7/task2/TestDrive.java
+++ lab7/task2/TestDrive.java
@@ -3,12 +3,16 @@ public class TestDrive {
 	public static void main(String[] args) {
 		
 		// what about a singleton here???
-		RemoteDeviceV1 remote = new RemoteDeviceV1();
+		// RemoteDeviceV1 remote = new RemoteDeviceV1();
+		RemoteDevice remote = new RemoteDevice();
 		
 		remote.pressPlay();
 		
 		remote.pressPause();
+		remote.pressLocked();
 		
+		remote.pressStop();
+		remote.pressLocked();
 		remote.pressStop();
 		
 		remote.pressPlay();
