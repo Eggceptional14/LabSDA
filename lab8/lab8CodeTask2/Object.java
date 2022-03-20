0465b7e0f21202b521aa12caf3a7daa4e1746785
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-03-20 16:40:07 +0700

    Update Object class

--- lab8/lab8CodeTask2/Object.java
+++ lab8/lab8CodeTask2/Object.java
@@ -1,5 +1,22 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 public class Object extends Object_Component {
-    
+    private ArrayList<Object_Component> list = new ArrayList<Object_Component>();
+
+    @Override
+    public void render() {
+        
+    }
+
+    @Override
+    public float volume() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Iterator createIterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
