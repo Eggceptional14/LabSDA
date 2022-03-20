6822bc37179ae0f7cc44f2ee5141303c38482bb9
Phurit Warapattanapong <phuritwarapat@gmail.com>
2022-03-20 23:52:53 +0700

    fix iterator

--- lab8/lab8CodeTask1/DinerMenuIterator.java
+++ lab8/lab8CodeTask1/DinerMenuIterator.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class DinerMenuIterator implements Iterator<MenuItem>{
+public class DinerMenuIterator implements Iterator{
     private MenuItem[] menuItems;
     private int curIndex;
 
@@ -15,8 +15,7 @@ public class DinerMenuIterator implements Iterator<MenuItem>{
 
     public MenuItem next() {
         if (this.hasNext()) {
-            this.curIndex++;
-            return this.menuItems[this.curIndex];
+            return this.menuItems[this.curIndex++];
         }
         else {
             return null;
