8022c2c05dae2ea75c7cc087e6755348e5333d3f
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-03-20 19:47:47 +0700

    Update render and volume function

--- lab8/lab8CodeTask2/Object.java
+++ lab8/lab8CodeTask2/Object.java
@@ -2,24 +2,32 @@ import java.util.ArrayList;
 import java.util.Iterator;
 public class Object extends Object_Component {
     private ArrayList<Object_Component> list = new ArrayList<Object_Component>();
-    private float o_volume ;
     
+    public Object() {}
+
     public void add(Object_Component object_component) {
         this.list.add(object_component);
-        this.o_volume += list.volume();
-
     }
 
     @Override
     public void render() {
-        Iterator iterator = list.iterator();
-        for(Object_Component objectComp : list )
-            ((Object_Component) iterator.next()).render();
+        Iterator iterator = this.list.iterator();
+        if (iterator.hasNext()) {
+            Object_Component comp = (Object_Component)iterator.next();
+            comp.render();
+        }
     }
 
     @Override
     public float volume() {
-        return this.o_volume;
+        float total_volume = 0;
+        for (int i = 0; i < this.list.size(); i++) {
+            
+            Object_Component comp = (Object_Component)this.list.get(i);
+            total_volume += comp.volume();
+            System.out.println(i);
+        }
+        return total_volume;
     }
 
     @Override
