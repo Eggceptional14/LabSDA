f45aea8761467f847d16ec128b44d8f293cdcb11
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-03-20 15:34:14 +0700

    Add Object and Object iterator file

---
+++ lab8/lab8CodeTask2/Object_Component.java
@@ -0,0 +1,10 @@
+import java.util.Iterator;
+
+public abstract class Object_Component {
+    
+    public abstract void render();
+
+    public abstract float volume();
+
+    public abstract Iterator createIterator();
+}
