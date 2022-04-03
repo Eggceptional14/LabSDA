25895da158e6aaac01fd620a1b824be40199f646
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-04-03 15:12:20 +0700

    Create Email.java

---
+++ lab10/Email.java
@@ -0,0 +1,15 @@
+public class Email {
+    private String type;
+
+    public Email(String type) {
+        setType(type);
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
