45700ec8496141f8f3b2d9d873e77679fab02b5f
Phurit Warapattanapong <phuritwarapat@gmail.com>
2022-04-22 23:44:01 +0700

    flyweight factory

--- lab12/lab12task1/IconFactory.java
+++ lab12/lab12task1/IconFactory.java
@@ -15,22 +15,34 @@ public class IconFactory {
     private Map iconmap = new HashMap();
 
     // Singleton.
-    // COMPLETE.
-
-
-
-
-
-
+    // COMPLETE
 
     public AbstractIcon createIcon(String key) {
 	// COMPLETE.
-
-
-
-
-
-
+        if ( !iconmap.containsKey(key.toLowerCase())) {
+            AbstractIcon newIcon;
+            switch (key.toLowerCase()) {
+                case "folder":
+                    newIcon = FolderIcon();
+                    break;
+                case "java":
+                    newIcon = JavaIcon();
+                    break;
+                case "pdf":
+                    newIcon = PdfIcon();
+                    break;
+                case "picture":
+                    newIcon = PictureIcon();
+                    break;
+                case "text":
+                    newIcon = TextIcon();
+                    break;
+                default:
+                    newIcon = UnknownIcon();
+            }
+            iconmap.put(key.toLowerCase(), newIcon);
+        }
+        return (AbstractIcon) iconmap.get(key);
 
     }
 
